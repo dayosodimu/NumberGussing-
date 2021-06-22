02660a49a418b10c1398ee9a2730a8f9f1fd3ca8
desoulchild <33025335+dayosodimu@users.noreply.github.com>
2021-06-22 14:37:30 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            { // User to enter floor width
+                Console.WriteLine("Enter floor width");
+                int InchWidth = Convert.ToInt32(Console.ReadLine());
+
+                // user to enter floor lenght 
+                Console.WriteLine("Enter floor lenght");
+                int InchLength = Convert.ToInt32(Console.ReadLine());
+
+                // cost per 1 unit 
+                decimal totalwidthinFeet = (InchWidth / 12);
+                decimal totalLengthinFeet = (InchLength / 12);
+
+                
+
+                Console.ReadLine();
+
+                Console.ReadKey();
+
+            }
+        }
+
+    }
+}
